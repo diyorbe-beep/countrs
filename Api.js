d7c46e797b28ba91bbc48dc6d225467392143ca1
dior_coder <144696383+diyorbe-beep@users.noreply.github.com>
2024-06-21 21:23:16 +0500

    a

--- Api.js
+++ Api.js
@@ -1,10 +1,10 @@
-let Api = 'https://restcountries.com/v3.1/all'
+let Api = 'https://restcountries.com/v3.1/'
 const countriesRow = document.querySelector('.countries_row')
 
 
-async function senRequest() {
+async function senRequest(link) {
       try {
-            let res = await fetch(Api)
+            let res = await fetch(Api + link)
             if (!res.ok) {
                   throw new Error('Error')
             }
@@ -15,24 +15,37 @@ async function senRequest() {
       }
 }
 
-senRequest()
+senRequest('all')
 
 function makeCountry(countries) {
+      countriesRow.innerHTML = ' '
       countries.forEach(country => {
-      console.log(country);
+            let capitalCounty
+            capitalCounty = country.capital ? country.capital.join(' / ') : '<span style="color: red;">No Capital</span></li>'
+            console.log(country.capital);
             let a = document.createElement('a')
-      a.classList.add('country')
-      a.innerHTML = `
+            a.classList.add('country')
+            a.innerHTML = `
       <img class="country_img" src="${country.flags.png}" alt="">
       <div class="info">
       <h1 class="country_title">${country.name.common}</h1>
       <ul class="country_list">
       <li class="country_list_item">Population: <span>${country.population}</span></li>
       <li class="country_list_item">Region: <span>${country.region}</span></li>
-      <li class="country_list_item">Capital: <span>Berlin</span></li>
+      <li class="country_list_item">Capital: <span>${capitalCounty}</span></li>
       </ul>
       </div>
       `
-      countriesRow.appendChild(a)
+            countriesRow.appendChild(a)
       });
 }
+
+
+const select = document.querySelector('.select')
+
+select.addEventListener('change', () => {
+      senRequest(`region/${select.value}`)
+      if (select.value == 'all') {
+            senRequest('all')
+      }
+})
